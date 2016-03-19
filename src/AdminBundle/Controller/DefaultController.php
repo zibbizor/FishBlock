bba639800eba261fc926a9397f0c2654895c9f77
AlleyLaptop <anthrax24@gmail.com>
2016-03-19 09:39:37 +0100

    Added the foundations of the admin panel

--- src/AdminBundle/Controller/DefaultController.php
+++ src/AdminBundle/Controller/DefaultController.php
@@ -12,6 +12,6 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('AdminBundle:Default:index.html.twig');
+        return $this->render('AdminBundle:Dashboard:index.html.twig');
     }
 }
